163e375e1e9ffae00570d007f7ff3870daf27ec5
Mark Hougaard <mark@hougaard.email>
2023-05-25 23:09:06 +0200

    Bump version $NPM_VERSION

--- packages/comp/release.sh
+++ packages/comp/release.sh
@@ -9,8 +9,8 @@ fi
 
 npm version $1
 git add .
-git commit -m 'Bump version'
 NPM_VERSION=$(npm pkg get version --workspaces=false | tr -d \")
-git tag $NPM_VERSION
-echo "Bumped version to $NPM_VERSION"
+git commit -m 'Bump version $NPM_VERSION'
+git tag comp-v$NPM_VERSION
+echo "Bumped version to v$NPM_VERSION"
 git push && git push --tags
