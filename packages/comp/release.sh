98dcc7f135b66706cd60cf811979f6ff74a3a484
Mark Hougaard <mark@hougaard.email>
2023-05-25 22:36:08 +0200

    Bump version

---
+++ packages/comp/release.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+if [ "$1" != "major" ] && [ "$1" != "minor" ] && [ "$1" != "patch" ];
+then
+    echo "Could not release!"
+    echo "Usage: 'npm run release -- (major|minor|patch)'"
+    echo ""
+    exit 1
+fi
+
+NEW_VERSION=$(npm version $1)
+
+git add .
+git commit -m 'Bump version'
+git tag $NEW_VERSION
+echo "Bumped version to $NEW_VERSION"
+git push && git push --tags
